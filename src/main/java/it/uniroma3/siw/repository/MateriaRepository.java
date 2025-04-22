9ffb00b324e0cc10c00f40b852e19d5569825397
Stefano <ste.morelli1@stud.uniroma3.it>
2025-04-22 11:27:37 +0200

    Aggiunto repository per Materia

---
+++ src/main/java/it/uniroma3/siw/repository/MateriaRepository.java
@@ -0,0 +1,9 @@
+package it.uniroma3.siw.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import it.uniroma3.siw.model.Materia;
+
+public interface MateriaRepository extends CrudRepository<Materia, Long> {
+
+}
