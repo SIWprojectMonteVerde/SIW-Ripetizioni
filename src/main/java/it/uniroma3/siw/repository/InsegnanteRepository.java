8605283f1e99b106eac8dccf1d8e382c52a3276b
Stefano <ste.morelli1@stud.uniroma3.it>
2025-04-28 19:13:22 +0200

    Correzione di alcuni errori e aggiunta di altre funzioni utili per lo svolgimento di operazioni definite nei casi d'uso

--- src/main/java/it/uniroma3/siw/repository/InsegnanteRepository.java
+++ src/main/java/it/uniroma3/siw/repository/InsegnanteRepository.java
@@ -3,10 +3,9 @@ package it.uniroma3.siw.repository;
 import org.springframework.data.repository.CrudRepository;
 
 import it.uniroma3.siw.model.Insegnante;
-import java.util.List;
+
 
 
 public interface InsegnanteRepository extends CrudRepository<Insegnante, Long>{
-	
 	public Iterable<Insegnante> findByEmail(String email);
 }
--- src/main/java/it/uniroma3/siw/repository/PrenotazioneRepository.java
+++ src/main/java/it/uniroma3/siw/repository/PrenotazioneRepository.java
@@ -2,9 +2,12 @@ package it.uniroma3.siw.repository;
 
 import org.springframework.data.repository.CrudRepository;
 
-import it.uniroma3.siw.model.Annuncio;
+
+import it.uniroma3.siw.model.Disponibilita;
+import it.uniroma3.siw.model.Prenotazione;
 import it.uniroma3.siw.model.Utente;
 
-public interface PrenotazioneRepository extends CrudRepository<Annuncio, Long> {
-	public Iterable<Annuncio> findByUtente(Utente utente);
+public interface PrenotazioneRepository extends CrudRepository<Prenotazione, Long> {
+	public Iterable<Prenotazione> findByUtente(Utente utente);
+	public Iterable<Prenotazione> findByDisponibilita(Disponibilita disponibilita);
 }
