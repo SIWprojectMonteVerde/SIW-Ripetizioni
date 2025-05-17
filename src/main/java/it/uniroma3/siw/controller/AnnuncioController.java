e239af9ee5e78e05c7b4edd34070eaa955bf683d
Stefano <ste.morelli1@stud.uniroma3.it>
2025-05-17 12:15:21 +0200

    Aggiunta la classe AnnuncioService Prima bozza di controller per gli annunci

--- src/main/java/it/uniroma3/siw/controller/AnnuncioController.java
+++ src/main/java/it/uniroma3/siw/controller/AnnuncioController.java
@@ -1,4 +1,41 @@
 package it.uniroma3.siw.controller;
 
+import it.uniroma3.siw.model.Annuncio;
+import it.uniroma3.siw.service.AnnuncioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
 public class AnnuncioController {
+    @Autowired
+    private AnnuncioService annuncioService;
+
+
+    @GetMapping("/annunci")
+    public String mostraAnnunci(Model model) {
+        model.addAttribute("annunci", annuncioService.findAll());
+        return "annunci";
+    }
+    @GetMapping("/annunci/{id}")
+    public String mostraAnnuncio(@PathVariable("id") Long id, Model model) {
+        model.addAttribute("annunci", annuncioService.findById(id));
+        return "annuncio";
+    }
+    @GetMapping("/formAnnuncio")
+    public String formAnnuncio(Model model) {
+        model.addAttribute("annuncio", new Annuncio());
+        return "formAnnuncio";
+    }
+
+    @PostMapping("/annunci")
+    public String aggiungiAnnuncio(@ModelAttribute("annuncio") Annuncio annuncio, BindingResult bindingResult) {
+
+        return "redirect:annunci/"+annuncio.getId();
+    }
 }
