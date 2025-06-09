aba0cd53a976a1b9dfbac839bb8d10d075b30dfe
Stefano <ste.morelli1@stud.uniroma3.it>
2025-06-09 19:23:11 +0200

    Classi per la gestione delle credenziali Controller per la gestione di login, register etc..

---
+++ src/main/java/it/uniroma3/siw/service/CredentialsService.java
@@ -0,0 +1,36 @@
+package it.uniroma3.siw.service;
+
+
+
+
+import it.uniroma3.siw.model.Credentials;
+import it.uniroma3.siw.repository.CredentialsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class CredentialsService {
+
+    @Autowired
+    protected PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private CredentialsRepository credentialsRepository;
+
+    public Credentials getCredentialsByUsername(Long id) {
+        return credentialsRepository.findById(id).orElse(null);
+    }
+    public Credentials getCredentialsByUsername(String username) {
+        return credentialsRepository.findByUsername(username).orElse(null);
+    }
+    public Credentials saveCredentials(Credentials credentials) {
+        credentials.setRole(Credentials.STUDENT_ROLE);
+        credentials.setPassword(this.passwordEncoder.encode(credentials.getPassword()));
+        return credentialsRepository.save(credentials);
+    }
+    public Boolean credentialsExistsByUsername(String username) {
+        return credentialsRepository.findByUsername(username).isPresent();
+    }
+}
